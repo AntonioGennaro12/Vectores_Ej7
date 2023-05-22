9d5c5d9f644a70676f7ff6ba9aa2b912f614f4a2
Antonio Gennaro <antgennaro@hotmail.com>
2023-05-22 16:28:24 -0300

    Vectores Ej 7 P#2

--- vectores_ej7.js
+++ vectores_ej7.js
@@ -4,6 +4,15 @@ const numeros   = [1, 2, 3, 4, 5, 6];
 
 const TITULO    = "Titulo";
 
-for (let cont=0;cont<numeros.length;cont++) {
-    miDiv1.innerHTML += ` <h2>${TITULO} ${(cont+1)}</h2>`;
+for (let cont=1;cont<=numeros.length;cont++) {
+    miDiv1.innerHTML += ` <h2>${TITULO} ${cont}</h2>`;
+}
+
+// Parte II agrega nombres distintos
+// Defino un arreglo de Titulos distintos
+
+const MATERIAS   = ["Matematicas", "Lengua", "Fisica", "Quimica", "Historia", "Geografía"];
+
+for (let cont=0;cont < numeros.length; cont++) {
+    miDiv1.innerHTML += ` <h2>${(cont+1)} - ${MATERIAS[cont]}</h2>`;
 }
