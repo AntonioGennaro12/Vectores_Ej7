196fce96b4f3f7c288222b049455f503885822b5
Antonio Gennaro <antgennaro@hotmail.com>
2023-05-23 09:08:09 -0300

    Vectores Ej #7 PI y PII v2 mejorado

--- vectores_ej7.js
+++ vectores_ej7.js
@@ -1,18 +1,20 @@
+// Parte I genera Titulos "h*" de acuerdo al vector números recibido
 const miDiv1    = document.querySelector ("#mi-div1");
 
 const numeros   = [1, 2, 3, 4, 5, 6];
-
 const TITULO    = "Titulo";
 
+miDiv1.innerHTML = `<p>Vectores - Ejercicio Nro 7 Parte I</p>`;
 for (let cont=1;cont<=numeros.length;cont++) {
-    miDiv1.innerHTML += ` <h2>${TITULO} ${cont}</h2>`;
+    miDiv1.innerHTML += `<h${cont}>${TITULO} ${cont}</h${cont}>`;
 }
 
 // Parte II agrega nombres distintos
 // Defino un arreglo de Titulos distintos
+const miDiv2    = document.querySelector ("#mi-div2");
+const MATERIAS   = ["Matemáticas", "Castellano", "Geografía", "Historia", "Química", "Física"];
 
-const MATERIAS   = ["Matematicas", "Lengua", "Fisica", "Quimica", "Historia", "Geografía"];
-
-for (let cont=0;cont < numeros.length; cont++) {
-    miDiv1.innerHTML += ` <h2>${(cont+1)} - ${MATERIAS[cont]}</h2>`;
+miDiv2.innerHTML += `<p>Vectores - Ejercicio Nro 7 Parte II</p>`;
+for (let cont=1;cont <= numeros.length; cont++) {
+    miDiv2.innerHTML += `<h${cont}>${(cont)} - ${MATERIAS[cont-1]}</${cont}>`;
 }
